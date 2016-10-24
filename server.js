707904571b3c8baeca8077cbac7371f5e1c112c2
snobu <foo@snobu.org>
2016-10-24 13:08:23 +0300

    simpler request

--- server.js
+++ server.js
@@ -1,54 +1,20 @@
 var http = require('http');
 
 function call_onprem_server(response) {
-    var options = {
-      host: 'hybridconnmgr.hybridConnMgr.f6.internal.cloudapp.net',
-      //host: 'checkip.amazonaws.com',
-      path: '/'
-    };
-
-    callback = function(hcres) {
-        var str = '';
-
-        hcres.on('data', function (chunk) {
-            str += chunk;
-        });
-
-        hcres.on('error', function (error) {
-            console.error(error);
-            response.writeHead(200, {'Content-type': 'text-plain'});
-            response.write(error);
-            response.end();
-            console.log(str);
-        });
-
-        hcres.on('timeout', function (error) {
-            console.error(error);
-            response.writeHead(200, {'Content-type': 'text-plain'});
-            response.write(error);
-            response.end();
-            console.log(error);
-        });
-
-        // We have the entire response
-        hcres.on('end', function () {
-            response.writeHead(200, {'Content-type': 'text-plain'});
-            response.write(str);
-            response.end();
-            console.log(str);
-        });
-    }
-
-    // Make the request over Hybrid Connection
     try {
-        http.request(options, callback).end();
+        console.log('Making outbound request');
+        http.get('http://hybridconnmgr.hybridConnMgr.f6.internal.cloudapp.net', function (hcres) {
+            response.writeHead(200, {'Content-type': 'text-plain'});
+            response.write(hcres);
+            response.end();
+            console.log(hcres);
+        });
     }
     catch (error) {
-        console.error(error);
         response.writeHead(200, {'Content-type': 'text-plain'});
         response.write(error);
         response.end();
-        console.log('error in catch ' + error);
+        console.log(error);
     }
 }
 
